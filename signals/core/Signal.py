4996fcbc58efb2c7b551026844a0e08c77531e27
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-02-10 17:25:47 +0100

    Update Signal.py
    duration parameter if now fed first, so we can plot/play/to_wave easier
    example :
    signal.plot(duration=5) --> signal.plot(5)

--- signals/core/Signal.py
+++ signals/core/Signal.py
@@ -34,9 +34,11 @@ class Signal:
     def sample_data(self, time_sample):
         return tuple(self.i_sample_data(time_sample))
 
-    def sample(self, t_min: float = 0, t_max: float = None, duration: float = None, n_frames: int = None,
-               frame_rate: float = 44100.0):
-        time_sample = TimeSample(t_min=t_min, t_max=t_max, duration=duration, n_frames=n_frames, frame_rate=frame_rate)
+    def sample(self,
+               duration: float = None,
+               t_max: float = None, n_frames: int = None,
+               t_min: float = 0, frame_rate: float = 44100.0):
+        time_sample = TimeSample(duration=duration, t_min=t_min, t_max=t_max, n_frames=n_frames, frame_rate=frame_rate)
         return SignalSample.from_signal(
             time_sample=time_sample,
             signal=self
@@ -44,27 +46,30 @@ class Signal:
 
     def to_wave(self,
                 filepath: str,
-                t_min: float = 0, t_max: float = None, duration: float = None, n_frames: int = None,
-                frame_rate: float = 44100.0,
+                duration: float = None,
+                t_max: float = None, n_frames: int = None,
+                t_min: float = 0, frame_rate: float = 44100.0,
                 norm_to=None, sampwidth=2, bufsize=2048):
         """Sample methods made accessible from the signal directly (see the corresponding method definition in the SignalSample class)"""
-        self.sample(t_min=t_min, t_max=t_max, duration=duration, n_frames=n_frames, frame_rate=frame_rate) \
+        self.sample(duration=duration, t_min=t_min, t_max=t_max, n_frames=n_frames, frame_rate=frame_rate) \
             .to_wave(filepath=filepath, norm_to=norm_to, sampwidth=sampwidth, bufsize=bufsize)
 
     def play(self,
-             t_min: float = 0, t_max: float = None, duration: float = None, n_frames: int = None,
-             frame_rate: float = 44100.0,
+             duration: float = None,
+             t_max: float = None, n_frames: int = None,
+             t_min: float = 0, frame_rate: float = 44100.0,
              norm_to=None, sampwidth=2, bufsize=2048):
         """Sample methods made accessible from the signal directly (see the corresponding method definition in the SignalSample class)"""
-        self.sample(t_min=t_min, t_max=t_max, duration=duration, n_frames=n_frames, frame_rate=frame_rate) \
+        self.sample(duration=duration, t_min=t_min, t_max=t_max, n_frames=n_frames, frame_rate=frame_rate) \
             .play(norm_to=norm_to, sampwidth=sampwidth, bufsize=bufsize)
 
     def plot(self,
-             t_min: float = 0, t_max: float = None, duration: float = None, n_frames: int = None,
-             frame_rate: float = 44100.0,
+             duration: float = None,
+             t_max: float = None, n_frames: int = None,
+             t_min: float = 0, frame_rate: float = 44100.0,
              title: str = "", xlabel: str = "", ylabel: str = "", export_to: str = None):
         """Sample methods made accessible from the signal directly (see the corresponding method definition in the SignalSample class)"""
-        self.sample(t_min=t_min, t_max=t_max, duration=duration, n_frames=n_frames, frame_rate=frame_rate) \
+        self.sample(duration=duration, t_min=t_min, t_max=t_max, n_frames=n_frames, frame_rate=frame_rate) \
             .plot(xlabel=xlabel, ylabel=ylabel, title=title, export_to=export_to)
 
 
